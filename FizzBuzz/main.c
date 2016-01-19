4f342c368ec3baf57a54407c81a4263016a9f1f8
Jake Zeal <jakezeal@gmail.com>
2016-01-19 14:17:11 -0500

    Merge

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -6,10 +6,21 @@
 //  Copyright © 2016 Jake Zeal. All rights reserved.
 //
 
+
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    
+    for (int x = 1; x <= 100; x++) {
+        if (x % 15 == 0) {
+            printf("FizzBuzz\n");
+        } else if (x % 3 == 0) {
+            printf("Fizz\n");
+        } else if (x % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", x);
+        }
+    }
     return 0;
 }
